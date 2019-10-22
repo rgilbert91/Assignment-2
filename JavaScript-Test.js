e128834a43fbf5aa0351415fbc221c6e2b92edf7
Rasheem Gilbert <rasheem.gilbert@gmail.com>
2019-10-22 10:00:32 -0400

    assignment 2

--- JavaScript-Test.js
+++ JavaScript-Test.js
@@ -239,27 +239,78 @@ function findStr(str, long) {
 // taking two number bound as parameters and returns an array of 
 // every possible self dividing number between 
 // them, including the bounds. 
-// Hint: A self-dividing number is a number that is divisible by every digit it contains. For example, 128 is a self-dividing number because 128 % 1 ==0, 128 % 2 == 0 and 128 % 8 == 0. 
+// Hint: A self-dividing number is a number
+// that is divisible by every digit it contains.
+// For example, 128 is a self - dividing number 
+// because 128 % 1 == 0, 128 % 2 == 0 and 128 % 8 == 0. 
 // Examples:
 // 	selfDividingNumbers(1, 22) ========> [1,2,3,4,5,6,7,8,9,11,12,15,22]
 // 	selfDividingNumbers(1, 10) =======> [1,2,3,4,5,6,7,8,9]
 // selfDividingNumbers(12, 21) =======> [12, 15]
+function selfDividingNumbers(left, right) {
+  // loop from left to right
+  var solution = [];
+  for (var i = left; i <= right; i++) {
+    // check if current number is self-dividing number
+    if (checkSelfDividing(i)) {
+      solution.push(i);
+    }
+  }
+​
+  return solution;
+}
+​
+// helper function to check if the num is self-dividing
+function checkSelfDividing(num) {
+  // change number into string foramat
+  var numString = num.toString();
+  // Split string into array of char
+  var numArray = numString.split("");
+​
+  var length = numArray.length;
+  for (var i = 0; i < length; i++) {
+    // if num is not divisible by any digit
+    if (num % ParseInt(numArray[i]) !== 0) {
+      return false;
+    }
+  }
+  // if num is divisible by all digits
+  return true;
 
 
-
-
-
-// Question 11 Write the function moveZeros(nums) taking an array of numbers and move all 0’s to the end of it while maintaining the relative order of the non-zero elements.  For example:
+// Question 11 Write the function moveZeros(nums) 
+// taking an array of numbers and move all 0’s to 
+// the end of it while maintaining the relative order 
+// of the non - zero elements.For example:
 // moveZeros([0,1,0,3,12]) ======> [1,3,12,0,0]
 // moveZeros([1,2,0,0,2,312,33,0,0]) ======> [1,2,2,312,33,0,0,0,0]
 // moveZeros([0,0,0]) ======> [0,0,0]
 // moveZeros([1,2,312,11,2]) =======> [1,2,312,11,2]
+function moveZeros(nums) {
+  // Loop through the whole array
+  var length = nums.length
+  var solution = [];
+  var counter = 0
+  for (var i = 0; i < length; i++){
+    if (nums[i] === 0) {
+      counter++;
+    } else {
+      solution.push(nums[i])
+    }
+  }
+  // adding zeros
+  while (counter > 0) {
+    solution.push(0);
+    counter--
+  }
+  return solution;
+}
 
 
 
 
-
-// // Question 12 12. Create an average(nums) function that calculates the average of an array of numbers.
+// // Question 12 Create an average(nums) function that calculates 
+// the average of an array of numbers.
 // Examples: 
 // average([2, 6]) =====> 4
 // average([2, 3, 3, 5, 7, 10]) ======> 5
@@ -267,3 +318,12 @@ function findStr(str, long) {
 // average([]) ======> 0
 
 // Hint: Sum of all elements / number of elements
+
+function average(num) {
+  // loop for sum of all elements
+  var sum = 0;
+  for (var i = 0; i < num.length; i++) {
+    sum += num[i]
+  }
+  return sum / num.length
+}
